0323b78fa1cfc610c4c4abedfb3ece2d17cfb102
CODEBREAKERGAURAV <gy3098225@gmail.com>
2025-02-28 08:51:20 +0530

    1.1

--- foodorder/src/components/Body.js
+++ foodorder/src/components/Body.js
@@ -1,32 +1,55 @@
+import { useState, useEffect } from "react";
+import RestaurantCard from "./Restaurantcard";
 
-import RestroCard from "./RestroCard";
-import resObj from "../utils/mockdata";
-import { useEffect, useState } from "react";
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-const Body=()=>{
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-let [listOfRestaurants,setListOfRestraunt]=useState(resObj);
-  return(
-    <div className="body">
-      <div className='filter'>
-       <button className="filter-btn" onClick={
-        ()=>{
-          const filterList=listOfRestaurants.filter(
-            (res)=>res.avgRating>4
-          );
-        }
-       }>Top Rated Restaurants</button>
-      </div>
-      <div className='res-container'>
-
+  const fetchData = async () => {
     
-             
-      </div>
-     
+      const data = await fetch(
+        "https://www.swiggy.com/mapi/restaurants/list/v5?lat=25.3176452&lng=82.9739144&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&carousel=true&third_party_vendor=1"
+      );
+      const json = await data.json();
 
+      console.log(json);
+      //optional chaining
+      setListOfRestaurants(json?.data?.cards?.[2]?.data?.data?.cards || []);
+    
+  };
+
+ 
+
+  return (
+    <div className="body">
+      {/* <div className="search-container">
+        <input type="text" placeholder="Search Food or Restaurant" />
+        <button>Search</button>
+      </div> */}
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res?.data?.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant?.data?.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
-   
-  )
+  );
 };
 
 export default Body;
--- foodorder/src/components/Header.js
+++ foodorder/src/components/Header.js
@@ -1,23 +1,21 @@
+import { LOGO_URL } from '../utils/constants';
+
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk-PySZkjQ-Jh2Bwno1bQOfVb5OLgbxpiikQ&s"
-          alt="Logo"
-        />
-      </div>
-      <div className="nav-item">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src={LOGO_URL} alt="App Logo" className="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
